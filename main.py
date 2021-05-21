f04dc0b6b0a2b0d2fb29df8fa5310167a58328b5
Borkar <ts-swarnim.borkar@p81305.intra.rakuten.co.jp>
2021-05-21 14:51:06 +0900

    chose theme for whole project: Pokemon Storage

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/')
+#db connection goes here
+db = []
+
+#models
+class PokeBank(BaseModel):
+    pk_name: str
+    level: int
+    nick: str
+
+
+#index/landing
+@app.get('/') 
 def index():
     return {'key':'value'}
+    
+
+#gets all
+@app.get('/pokemon')
+def get_pokemon():
+    return db
+
+
+## might wanna call this caught
+# @app.post('/pokemon')
+
+
+## might wanna call this release
+# @app.delete('/pokemon')
+
+
+
+
+# @app.get('/pokemon/{poke_name}')
+
 
