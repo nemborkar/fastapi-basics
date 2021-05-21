496c29b5445073e2ccbce3cb545289167f41b7cb
Borkar <ts-swarnim.borkar@p81305.intra.rakuten.co.jp>
2021-05-21 16:13:19 +0900

    added functionality to check, store and release pokemon

--- main.py
+++ main.py
@@ -3,35 +3,42 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-#db connection goes here
+# db connection goes here?
+# db is a list!
 db = []
 
-#models
-class PokeBank(BaseModel):
-    pk_name: str
-    level: int
+# models
+class PokePC(BaseModel):
+    name: str
     nick: str
+    level: int
+    
 
 
-#index/landing
+# index/landing
 @app.get('/') 
 def index():
     return {'key':'value'}
     
 
-#gets all
+# shows all pokemon in PC
 @app.get('/pokemon')
-def get_pokemon():
+def show_pokemon():
     return db
 
 
-## might wanna call this caught
-# @app.post('/pokemon')
+# might wanna call this caught
+@app.post('/pokemon')
+def store_pokemon(pokemon: PokePC): # pokemon here resembles something like the model PokePC
+    db.append(pokemon.dict()) # converts pokemon to dictionary and then appends to the database (list?)
+    return db[-1] # returns last one from the db
 
 
 ## might wanna call this release
-# @app.delete('/pokemon')
-
+@app.delete('/pokemon/{poke_id}')
+def release_pokemon(poke_id: int):
+    db.pop(poke_id-1)
+    return "Pokemon #"+str(poke_id)+" was released"
 
 
 
