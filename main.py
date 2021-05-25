d1e3b47204069927e02776790518a30f9bc98238
Borkar <ts-swarnim.borkar@p81305.intra.rakuten.co.jp>
2021-05-25 18:00:16 +0900

    added static database and got OAuth2 to work correctly

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 from fastapi import FastAPI, Depends
 from pydantic import BaseModel
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 app = FastAPI()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="derp_token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-# db connection goes here?
-# db is a list!
-db = []
+# db is a list, so can use statics values for now
+db = [
+    {
+        "name": "Magikarp",
+        "nick": "Kraken",
+        "level": 18
+    },
+    {
+        "name": "Gengar",
+        "nick": "Shade",
+        "level": 56
+    },
+    {
+        "name": "Pidgeot",
+        "nick": "aaabaaajss",
+        "level": 88
+    },
+    {
+        "name": "Bulbasaur",
+        "nick": "Vines",
+        "level": 5
+    }
+]
 
 # models
 class PokePC(BaseModel):
@@ -20,26 +40,36 @@ class PokePC(BaseModel):
 
 # index/landing
 @app.get("/") 
-def index(token: str = Depends(oauth2_scheme)):
-    return {"token": token}
+async def index(token: str = Depends(oauth2_scheme)):
+    return {"token_main": token}
+
+
+# token endpoint; token gets created here by default
+@app.post("/token") 
+async def token(form_data: OAuth2PasswordRequestForm = Depends()):
+    return {"access_token": form_data.username+"\'s token", "token_type": "bearer"}
     
+
+
 
 # shows all pokemon in PC
 @app.get("/pokemon")
-def show_pokemon():
+async def show_pokemon():
     return db
+
 
 
 # might wanna call this caught
 @app.post("/pokemon")
-def store_pokemon(pokemon: PokePC): # pokemon here resembles something like the model PokePC
+async def store_pokemon(pokemon: PokePC): # pokemon here resembles something like the model PokePC
     db.append(pokemon.dict()) # converts pokemon to dictionary and then appends to the database (list?)
     return db[-1] # returns last one from the db
 
 
+
 ## might wanna call this release
 @app.delete("/pokemon/{poke_id}")
-def release_pokemon(poke_id: int):
+async def release_pokemon(poke_id: int):
     db.pop(poke_id-1)
     return "Pokemon #"+str(poke_id)+" was released"
 
